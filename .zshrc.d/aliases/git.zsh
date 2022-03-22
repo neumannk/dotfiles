a9dad30fb937d5f7aa5f821ec2a51381cae737a1
Kevin Neumann <neukev@amazon.com>
2022-03-22 13:51:27 -0700

    add --no-pager to git log aliases

--- .zshrc.d/aliases/git.zsh
+++ .zshrc.d/aliases/git.zsh
@@ -1,4 +1,4 @@
-alias gg='git log --graph --all --decorate --simplify-by-decoration --oneline'
-alias ggv='git log --graph --oneline --decorate --all'
+alias gg='git --no-pager log --graph --all --decorate --simplify-by-decoration --oneline'
+alias ggv='git --no-pager log --graph --oneline --decorate --all'
 alias gs='git status'
 alias gtm='git branch -u origin/mainline'
